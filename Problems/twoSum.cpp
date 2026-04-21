7f067baa62ad7cb18e27865b6b4975e4dfd54844
Arwasoliman <152231359+Arwasoliman@users.noreply.github.com>
2026-04-21 12:11:45 -0400

    add testecase to two sum problem

--- Problems/twoSum.cpp
+++ Problems/twoSum.cpp
@@ -22,3 +22,35 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }
     return {};
 }
+
+int main(){
+
+    // Test Case 1:
+    vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
+    vector<int> result1 = twoSum(nums1, target1);
+    cout << "Test Case 1: ";
+    for (int i=0; i < result1.size(); i++) 
+        cout << result1[i] << " "; // Expected output: [0, 1]
+    cout << endl;
+
+    // Test Case 2:
+    vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    vector<int> result2 = twoSum(nums2, target2);
+    cout << "Test Case 2: ";
+    for (int i=0; i < result2.size(); i++) 
+        cout << result2[i] << " "; // Expected output: [1, 2]
+    cout << endl;
+
+    // Test Case 3:
+    vector<int> nums3 = {3, 3};
+    int target3 = 6;
+    vector<int> result3 = twoSum(nums3, target3);
+    cout << "Test Case 3: ";
+    for (int i=0; i < result3.size(); i++) 
+        cout << result3[i] << " "; // Expected output: [0, 1]
+    cout << endl;
+
+    return 0;
+}
